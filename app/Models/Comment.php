a14acf40679b172770073c7982653f76a492058c
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2024-02-25 16:35:36 +0100

    Mise en place des relations dans les modèles (Comment, Post, Role, User) + commentaires

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    // Définition d'une relation "belongsTo" avec le modèle Post
+    // Un commentaire appartient à un seul article (Post)
+    // Cardinalité : 1 (Comment) vers 1 (Post)
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    // Définition d'une relation "belongsTo" avec le modèle User
+    // Un commentaire appartient à un seul utilisateur (User)
+    // Cardinalité : 1 (Comment) vers 1 (User)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
